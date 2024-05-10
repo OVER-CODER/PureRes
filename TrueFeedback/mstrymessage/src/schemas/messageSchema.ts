a72e412d6c89f205055a6d878ffe0d58236787a0
Aryan Pandit <aryanpandit.1205@gmail.com>
2024-05-10 17:22:10 +0530

    TrueFeedback

---
+++ TrueFeedback/mstrymessage/src/schemas/messageSchema.ts
@@ -0,0 +1,9 @@
+import { Content } from 'next/font/google';
+import {z} from 'zod';
+
+export const MessageSchema = z.object({
+    content: z
+    .string()
+    .min(10, "Message must be at least 10 characters long")  
+    .max(300, "Message must be at most 300 characters long")
+})
